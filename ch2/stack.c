2d922228965237d488379fbf18065f3edd6ae333
rafael F. Katopodis <rafaelkatopodis@gmail.com>
2017-05-11 10:06:03 -0300

    Add pointer alignment check

--- ch2/stack.c
+++ ch2/stack.c
@@ -4,6 +4,7 @@
 #include "stack.h"
 
 #define T Stack_T
+#define isBadPtr(p) ((int) p)%16 == 0 // Checking pointer alignment (Stack_T has 12 bytes of size + 4 bytes of padding)
 
 struct T {
     int count;
@@ -27,6 +28,7 @@ T Stack_new(void) {
 
 int Stack_empty(T stk) {
     assert(stk && stk->mark == 0xAB);
+    assert(isBadPtr(stk));
     return stk->count == 0;
 }
 
@@ -34,6 +36,8 @@ void Stack_push(T stk, void *x) {
     struct elem *t;
     
     assert(stk && stk->mark == 0xAB);
+    assert(isBadPtr(stk));
+
     NEW(t);
     t->x = x;
     t->link = stk->head;
@@ -45,6 +49,7 @@ void *Stack_pop(T stk) {
     struct elem *t;
     void *x;
     assert(stk && stk->mark == 0xAB);
+    assert(isBadPtr(stk));    
     assert(stk->count > 0);
 
     t = stk->head;
@@ -60,7 +65,9 @@ void *Stack_pop(T stk) {
 void Stack_free(T *stk) {
     struct elem *t, *u;
 
-    assert(stk && stk->mark == 0xAB && *stk);
+    assert(stk && (*stk)->mark == 0xAB && *stk);
+    assert(isBadPtr(stk));
+    
     for(t = (*stk)->head; t; t = u) {
         u = t->link;
         FREE(t);
